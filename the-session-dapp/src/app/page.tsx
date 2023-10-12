e944a5c5db52eaafec65b645be8f9010c144d135
eduairet <hola@eduairet.com>
2023-10-11 21:50:15 -0600

    Responsiveness improvement

--- the-session-dapp/src/app/page.tsx
+++ the-session-dapp/src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <main className='mx-auto flex min-h-screen max-w-[1080px] flex-col items-center justify-between px-12 pb-24 pt-12'>
       <img src={TheSessionLogo.src} alt='logo' className='max-w-[200px]' />
-      <section className='flex flex-col gap-[3rem] py-10'>
-        <h1 className='max-w-[450px] text-center text-[6rem] font-black leading-none'>
+      <section className='flex flex-col gap-[clamp(1rem,3vw,3rem)] pb-16 pt-12'>
+        <h1 className='text-balance max-w-[450px] text-center text-[clamp(3.5rem,10vw,6rem)] font-black leading-none'>
           let’s talk about it
         </h1>
-        <p className='text-center text-[1.5rem] text-lg font-bold'>
+        <p className='text-balance text-center text-[clamp(1rem,3vw,1.5rem)] text-lg font-bold'>
           Find your favorite group therapy
         </p>
       </section>
