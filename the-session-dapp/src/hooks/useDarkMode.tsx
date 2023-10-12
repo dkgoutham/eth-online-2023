689e97aeabfd3a7c64e53864e00404c2f1842ccb
eduairet <hola@eduairet.com>
2023-10-11 22:43:16 -0600

    DarkMode Hook

---
+++ the-session-dapp/src/hooks/useDarkMode.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+export function useDarkMode(): boolean {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+    const handleDarkModeChange = (event: any) => {
+      setIsDarkMode(event.matches)
+    }
+
+    darkModeMediaQuery.addEventListener('change', handleDarkModeChange)
+    setIsDarkMode(darkModeMediaQuery.matches)
+
+    return () => {
+      darkModeMediaQuery.removeEventListener('change', handleDarkModeChange)
+    }
+  }, [])
+
+  return isDarkMode
+}
