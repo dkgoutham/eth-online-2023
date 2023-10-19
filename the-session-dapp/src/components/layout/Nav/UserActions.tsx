0b83de1286ae48a17086dce7a4bdc2144ef5b925
eduairet <hola@eduairet.com>
2023-10-18 23:26:16 -0600

    Connect modal when clicking in user

--- the-session-dapp/src/components/layout/Nav/UserActions.tsx
+++ the-session-dapp/src/components/layout/Nav/UserActions.tsx
@@ -1,19 +1,21 @@
 'use client'
 
+import { useAccountModal } from '@rainbow-me/rainbowkit'
 import Badge from './Badge'
 import Avatar from './Avatar'
 import ConnectButton from '../ConnectButton'
 import { useConnectContext } from '@/store'
 
 export default function UserActions() {
-  const { isConnected, user, disconnect } = useConnectContext()
+  const { isConnected, user } = useConnectContext()
+  const { openAccountModal } = useAccountModal()
 
   if (user && isConnected) {
     const { id, username, avatar, notifications } = user
     return (
       <li
         className='flex cursor-pointer items-center gap-4'
-        onClick={() => disconnect()}
+        onClick={openAccountModal}
       >
         <span className='hidden max-w-[10ch] truncate sm:inline'>
           hi {username || id}!
