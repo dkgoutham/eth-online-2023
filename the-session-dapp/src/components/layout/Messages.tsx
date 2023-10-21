6546ee2fadbebc7b1e54a75e0e1e111d5a6dad39
eduairet <hola@eduairet.com>
2023-10-21 13:39:48 -0600

    Messages padding fix

--- the-session-dapp/src/components/layout/Messages.tsx
+++ the-session-dapp/src/components/layout/Messages.tsx
@@ -31,28 +31,32 @@ export default function Messages({ push }: IProps) {
   }
 
   return (
-    <section className='relative h-[50vh] min-h-[300px] w-full overflow-y-scroll rounded-[10vh] border-[1px] border-[--black] py-3 text-center dark:border-[--white]'>
+    <section className='relative h-[50vh] min-h-[300px] w-full overflow-hidden rounded-[10vh] border-[1px] border-[--black] text-center dark:border-[--white]'>
       <div className='absolute left-0 top-0 h-[50px] w-full border-b-[1px] border-b-[--black] bg-[--background] pb-3 pt-4 text-[12px] dark:border-b-[--white]'>
         <time>{formatDate(new Date())}</time>
       </div>
-      <div className='mt-[50px] flex w-full flex-col items-start gap-1 p-3'>
+      <div className='pb-[40px] pt-[60px] flex w-[90%] flex-col items-start gap-1 overflow-y-scroll'>
         {isChatLoading && <Spinner />}
-        {[welcomeMessage, ...chat].map((el: GroupMessage, i: number) => {
-          const classes = [
-            'align-start rounded-full flex flex-col gap-[4px] p-3 text-sm',
-            el.isSender ? 'bg-[--sender] self-end' : 'bg-[--recipient]',
-          ].join(' ')
-          return (
-            <div key={`chat-message-${i}`} className={classes}>
-              {el.text}
-              {/* TODO Add time logic {el.time && (
+        {[welcomeMessage, ...(!!chat?.length ? chat : [])].map(
+          (el: GroupMessage, i: number) => {
+            const classes = [
+              'align-start rounded-full flex flex-col gap-[4px] p-4 text-sm max-w-[50%]',
+              el.isSender
+                ? 'bg-[--sender] self-end text-right'
+                : 'bg-[--recipient] text-left',
+            ].join(' ')
+            return (
+              <div key={`chat-message-${i}`} className={classes}>
+                {el.text}
+                {/* TODO Add time logic {el.time && (
                   <time className='align-end text-[10px]'>
                     {getHourAndMinutes(el.time)}
                   </time>
                 )} */}
-            </div>
-          )
-        })}
+              </div>
+            )
+          }
+        )}
       </div>
     </section>
   )
